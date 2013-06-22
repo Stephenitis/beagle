c608bf55299f1e5bd7613dda19b52bd6b85e040e
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-06-22 04:05:14 -0700

    Results from search displayed on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,6 @@ class HomeController < ApplicationController
 	QUERY_STRING = "https://congress.api.sunlightfoundation.com/bills?"
 require 'json'
 	def home_page
-		result = QUERY_STRING
-		redirect_to(root_path) if params.empty?
-		result << "query=#{params[:query]}"
 	end
 
   def search
@@ -15,11 +12,12 @@ require 'json'
     faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
   end
 
-  @response = conn.get("/bills?query=immigration&apikey=ebbcfb111bdb4b82a72694e10b776ae9")
+  @response = conn.get("/bills/search?query=#{params["query"]}&apikey=ebbcfb111bdb4b82a72694e10b776ae9")
   @response = JSON.parse(@response.body)
 
+  render :json => { :searchResponses => render_to_string(:partial => 'search_results'), :locals => {:response => @response}}
+
+  # render :json => { :menu_template => render_to_string(:partial => 'menu', :locals => {:menu => @menu}) }
 end
-
-
  # "http://congress.api.sunlightfoundation.com/bills?query=" + params[:query]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Beagle::Application.routes.draw do
   get "widget/show"
 
-  post '/search_bills' => 'billssearch#search_sunlight_for_bill'
   # post '/search_bills' => 'billssearch#search_sunlight_for_bill'
   root to: "home#home_page"
-  post "/search_bills" => "home#search"
+  post "/search" => "home#search"
 end

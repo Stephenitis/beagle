b3da4c5db3d59de6e01415385b3b1e8feec0f836
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-06-22 04:02:43 -0700

    Home page changed for results to be displayed via Ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,22 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+var Results = {
+  init: function() {
+    $('form').on('ajax:success', this.appendResults);
+    $('form').on('ajax:error', this.appendErrors);
+  },
+
+  appendResults: function(event, data, status, xhr) {
+    $('.results').html(data.searchResponses);
+  },
+
+  appendErrors: function(event, xhr, status, error) {
+    $('.results').before($.parseJSON(xhr.responseText).error);
+  }
+};
+
+$(document).ready(function() {
+  Results.init();
+});

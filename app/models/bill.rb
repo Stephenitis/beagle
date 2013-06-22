0222b6c1299c0e247ef904754a5ed5c41a025815
sidney zhang <sidazhang89@gmail.com>
2013-06-22 12:50:43 -0700

    Updated Model Accessibility

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,5 +1,5 @@
 class Bill < ActiveRecord::Base
-  attr_accessible :bill_id
+  attr_accessible :bill_id, :bill_type, :number, :congress, :introduced_on, :last_action_at, :last_vote_at, :official_title, :popular_title, :short_title, :congress_url, :govtrack_url, :opencongress_url, :active, :active_at, :awaiting_signature, :enacted, :enacted_at, :house_passage_result, :house_passage_result_at, :senate_passage_result, :senate_passage_result_at, :vetoed, :nicknames
   has_many :updates
   def to_param
     bill_id

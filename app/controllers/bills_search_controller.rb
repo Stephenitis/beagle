8fd4d5a174d25a002b5c790a54b0d4e27ce67bcc
Jared Smith <jared.rit@me.com>
2013-06-21 22:38:30 -0700

    Created bill_search_controller and put in the api key and url for searching. new file:   app/controllers/bills_search_controller.rb

---
+++ app/controllers/bills_search_controller.rb
@@ -0,0 +1,10 @@
+
+class BillsSearchController < ApplicationController
+	QUERY_STRING = "http://congress.api.sunlightfoundation.com/"
+	API_KEY = "ebbcfb111bdb4b82a72694e10b776ae9"
+
+	def search_sunlight_for_bill
+		
+	end
+
+end

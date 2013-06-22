14f6160cab5849a178a5127d8bb68823316b043b
Paul Mederos <paulmederos@me.com>
2013-06-22 13:41:49 -0700

    Modal is styled. Click on the Beagle logo to see an example of the modal.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function() {
+  $('.home-logo').click(function(){
+    $(".modal-container").fadeIn();
+  });
+
+  $(".modal-container").click(function(){
+    $(this).fadeOut('fast');
+  });
+});

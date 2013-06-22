3c57d76404ba42430aaf99412c578158fdcf96cb
sidney zhang <sidazhang89@gmail.com>
2013-06-22 10:30:10 -0700

    Iron Worker with bill creation

--- workers/hello_worker.rb
+++ workers/hello_worker.rb
@@ -1,9 +1,14 @@
+require 'active_record'
+require 'pg'
+require 'models/bill.rb'
+
 # Worker code can be anything you want.
 puts "Starting HelloWorker at #{Time.now}"
 puts "Payload: #{params}"
 puts "Simulating hard work for 5 seconds..."
 5.times do |i|
-  puts "Sleep #{i}..."
+  bill = Bill.create(bill_id: i)
+  p bill
   sleep 1
 end
 puts "HelloWorker completed at #{Time.now}"

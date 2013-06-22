c39a1fd28ad1a1964252b91efa9b6e291220b226
sidney zhang <sidazhang89@gmail.com>
2013-06-22 01:21:03 -0700

    Generated boilerplate code for widget controller

--- app/controllers/bill_searches_controller.rb
+++ app/controllers/bill_searches_controller.rb
@@ -1,4 +1,3 @@
-
 class BillSearchesController < ApplicationController
 	API_KEY = "ebbcfb111bdb4b82a72694e10b776ae9"
 	QUERY_STRING = "https://congress.api.sunlightfoundation.com/bills?"
@@ -8,7 +7,7 @@ class BillSearchesController < ApplicationController
 		redirect_to(root_path) if params.empty?
 
 		result << "query=#{params[:query]}"
-		
+
 		render :json => {:result => result[0..-2]}
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Beagle::Application.routes.draw do
+  get "widget/show"
+
   post '/search_bills' => 'billssearch#search_sunlight_for_bill'
 end

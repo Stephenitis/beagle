23b4bc9cab16dd4a6af3f2f03158ac0736b6ec72
Dexter V <dvubox@gmail.com>
2013-06-21 22:16:58 -0700

    initial skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beagle::Application.config.secret_token = 'efa40bfab544682c8bc3e060c7e8e678cdcbbc225c10ab29651183b23839fe4ec74c393c0d5df4f5c8f1223d528984ffe750368dcd02c8dc454a9facb734de23'

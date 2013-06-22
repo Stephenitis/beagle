cef19bf388d3452ebd9650d252f6e22c125efab1
sidney zhang <sidazhang89@gmail.com>
2013-06-22 10:38:08 -0700

    Changed WidgetController to WidgetsController

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,4 +1,4 @@
-class WidgetController < ApplicationController
+class WidgetsController < ApplicationController
   def show
     @bill = Bill.where(bill_id: params[:bill_id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Beagle::Application.routes.draw do
-  get "widget/show"
-
-  resources :widget, :only => [:show, :create]
+  resources :widgets, :only => [:show, :create]
   # post '/search_bills' => 'billssearch#search_sunlight_for_bill'
   root to: "home#home_page"
   post "/search" => "home#search"
